2b368b82d23c69c9da0ecb253edecf6dff088e2c
Amanda Sanjuan Sanchez <amanda_ss@Amandas-MacBook-Pro.local>
2024-09-19 17:26:00 -0600

    Exer 2 done done

--- DecisionTree/ML-exercise2-H1.py
+++ DecisionTree/ML-exercise2-H1.py
@@ -99,13 +99,75 @@ Tree_2_gini = decisiontree.ID3(S, Attributes, Label, "gini", 2)
 Tree_1_gini = decisiontree.ID3(S, Attributes, Label, "gini", 1)
 # print("Tree_1_gini done")
 
-######################################################## PREDICT AND COMPARE
+######################################################## PREDICT AND COMPARE TEST DATA SETS
+print("Prediction error for the test data set: ")
 
 total_exaples_to_test = 728
 pred_error_info = [0,0,0,0,0,0]
 pred_error_me = [0,0,0,0,0,0]
 pred_error_gini = [0,0,0,0,0,0]
 CSVfile = 'car/test.csv'
+with open(CSVfile, 'r') as f:
+    for line in f:
+        example_to_test = line.strip().split(',')
+        test = {"buying":example_to_test[0],"maint":example_to_test[1],"doors":example_to_test[2],"persons":example_to_test[3],"lug_boot":example_to_test[4],"safety":example_to_test[5]}
+        # info
+        if (decisiontree.prediction(Tree_1_info,test) != example_to_test[6]):
+            pred_error_info[0] += 1/total_exaples_to_test
+        if (decisiontree.prediction(Tree_2_info,test) != example_to_test[6]):
+            pred_error_info[1] += 1/total_exaples_to_test
+        if (decisiontree.prediction(Tree_3_info,test) != example_to_test[6]):
+            pred_error_info[2] += 1/total_exaples_to_test
+        if (decisiontree.prediction(Tree_4_info,test) != example_to_test[6]):
+            pred_error_info[3] += 1/total_exaples_to_test
+        if (decisiontree.prediction(Tree_5_info,test) != example_to_test[6]):
+            pred_error_info[4] += 1/total_exaples_to_test
+        if (decisiontree.prediction(Tree_6_info,test) != example_to_test[6]):
+            pred_error_info[5] += 1/total_exaples_to_test
+        # me
+        if (decisiontree.prediction(Tree_1_me,test) != example_to_test[6]):
+            pred_error_me[0] += 1/total_exaples_to_test
+        if (decisiontree.prediction(Tree_2_me,test) != example_to_test[6]):
+            pred_error_me[1] += 1/total_exaples_to_test
+        if (decisiontree.prediction(Tree_3_me,test) != example_to_test[6]):
+            pred_error_me[2] += 1/total_exaples_to_test
+        if (decisiontree.prediction(Tree_4_me,test) != example_to_test[6]):
+            pred_error_me[3] += 1/total_exaples_to_test
+        if (decisiontree.prediction(Tree_5_me,test) != example_to_test[6]):
+            pred_error_me[4] += 1/total_exaples_to_test
+        if (decisiontree.prediction(Tree_6_me,test) != example_to_test[6]):
+            pred_error_me[5] += 1/total_exaples_to_test
+        # gini
+        if (decisiontree.prediction(Tree_1_gini,test) != example_to_test[6]):
+            pred_error_gini[0] += 1/total_exaples_to_test
+        if (decisiontree.prediction(Tree_2_gini,test) != example_to_test[6]):
+            pred_error_gini[1] += 1/total_exaples_to_test
+        if (decisiontree.prediction(Tree_3_gini,test) != example_to_test[6]):
+            pred_error_gini[2] += 1/total_exaples_to_test
+        if (decisiontree.prediction(Tree_4_gini,test) != example_to_test[6]):
+            pred_error_gini[3] += 1/total_exaples_to_test
+        if (decisiontree.prediction(Tree_5_gini,test) != example_to_test[6]):
+            pred_error_gini[4] += 1/total_exaples_to_test
+        if (decisiontree.prediction(Tree_6_gini,test) != example_to_test[6]):
+            pred_error_gini[5] += 1/total_exaples_to_test
+
+
+print("Predicted error with information gain: ")
+print(pred_error_info)
+print("Predicted error with me gain: ")
+print(pred_error_me)
+print("Predicted error with gini gain: ")
+print(pred_error_gini)
+
+
+######################################################## PREDICT AND COMPARE TEST DATA SETS
+print("Prediction error for the train data set: ")
+
+total_exaples_to_test = 1000
+pred_error_info = [0,0,0,0,0,0]
+pred_error_me = [0,0,0,0,0,0]
+pred_error_gini = [0,0,0,0,0,0]
+CSVfile = 'car/train.csv'
 with open(CSVfile, 'r') as f:
     for line in f:
         example_to_test = line.strip().split(',')
